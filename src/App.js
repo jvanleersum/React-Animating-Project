8eb306cba794b34fc72c4c3ffd438d3c55c87be2
Judith van Leersum <judithvleersum@gmail.com>
2022-08-03 13:34:56 +0200

    Using React Transition to transition a red div

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Transition } from "react-transition-group";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
@@ -7,21 +8,46 @@ import "./App.css";
 
 const App = () => {
   const [showModal, setShowModal] = useState(false);
+  const [showBlock, setShowBlock] = useState(false);
 
   const showModalHandler = () => {
     setShowModal(true);
-  }
+  };
 
   const closeModalHandler = () => {
     setShowModal(false);
-  }
+  };
+
+  const blockHandler = () => {
+    setShowBlock((prevState) => !prevState);
+  };
 
   return (
     <div className="App">
       <h1>React Animations</h1>
-      <Modal show={showModal} closed={closeModalHandler}/>
-      <Backdrop show={showModal} closed={closeModalHandler}/>
-      <button className="Button" onClick={showModalHandler}>Open Modal</button>
+      <button onClick={blockHandler} className="Button">
+        Toggle
+      </button>
+      <br />
+      <Transition in={showBlock} timeout={300} mountOnEnter unmountOnExit>
+        {(state) => (
+          <div
+            style={{
+              backgroundColor: "red",
+              width: 100,
+              height: 100,
+              margin: "auto",
+              transition: "opacity 1s ease-out",
+              opacity: state === 'exiting' ? 0 : 1
+            }}
+          />
+        )}
+      </Transition>
+      {showModal && <Modal show={showModal} closed={closeModalHandler} />}
+      {showModal && <Backdrop show={showModal} closed={closeModalHandler} />}
+      <button className="Button" onClick={showModalHandler}>
+        Open Modal
+      </button>
       <h3>Animating Lists</h3>
       <List />
     </div>
